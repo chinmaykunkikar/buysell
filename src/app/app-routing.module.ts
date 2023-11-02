5bf7618f0dca48fd4c8be6036deb896a52c4a507
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-11-02 17:23:55 +0530

    Generate all required pages for the app and add them to the App router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { ContactPageComponent } from "src/app/contact-page/contact-page.component";
+import { EditListingPageComponent } from "src/app/edit-listing-page/edit-listing-page.component";
+import { ListingDetailPageComponent } from "src/app/listing-detail-page/listing-detail-page.component";
 import { ListingsPageComponent } from "src/app/listings-page/listings-page.component";
+import { MyListingsPageComponent } from "src/app/my-listings-page/my-listings-page.component";
+import { NewListingPageComponent } from "src/app/new-listing-page/new-listing-page.component";
 
 const routes: Routes = [
+  { path: "", redirectTo: "/listings", pathMatch: "full" },
   { path: "listings", component: ListingsPageComponent, pathMatch: "full" },
+  { path: "listings/:id", component: ListingDetailPageComponent },
+  { path: "contact/:id", component: ContactPageComponent },
+  { path: "edit-listing/:id", component: EditListingPageComponent },
+  { path: "my-listings", component: MyListingsPageComponent },
+  { path: "new-listing", component: NewListingPageComponent },
 ];
 
 @NgModule({
