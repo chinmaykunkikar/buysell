e0f3540beafb96f2a0538d6135a035d3254e082b
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-11-02 17:06:18 +0530

    Add ListingsPage using proper routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { ListingsPageComponent } from "src/app/listings-page/listings-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "listings", component: ListingsPageComponent, pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
