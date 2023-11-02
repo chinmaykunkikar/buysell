edeacc650ee7e718e4b705c45440da0376134b7c
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-11-02 20:17:41 +0530

    Implement listings page

--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,8 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Listing } from "src/app/types";
+import { fakeListings } from "src/app/fake-data";
 
 @Component({
   selector: "app-listings-page",
   templateUrl: "./listings-page.component.html",
   styleUrls: ["./listings-page.component.css"],
 })
-export class ListingsPageComponent {}
+export class ListingsPageComponent implements OnInit {
+  listings: Listing[] = [];
+
+  ngOnInit(): void {
+    this.listings = fakeListings;
+  }
+}
