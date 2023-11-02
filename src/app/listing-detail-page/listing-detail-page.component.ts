3f625c5ca17ded27cf9a9b390bce27cd82626716
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-11-02 20:17:54 +0530

    Implement Listing Details page

--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,8 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { fakeListings } from "src/app/fake-data";
+import { Listing } from "src/app/types";
 
 @Component({
   selector: "app-listing-detail-page",
   templateUrl: "./listing-detail-page.component.html",
   styleUrls: ["./listing-detail-page.component.css"],
 })
-export class ListingDetailPageComponent {}
+export class ListingDetailPageComponent implements OnInit {
+  listing: Listing | undefined;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get("id");
+    this.listing = fakeListings.find((listing) => listing.id === id);
+  }
+}
