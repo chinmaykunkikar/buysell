67cfea3f2211de91afb1f47ebfb02be8c9a4320e
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-11-02 17:00:08 +0530

    listings-page works! Generate new component using `ng generate component <name>`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ListingsPageComponent } from './listings-page/listings-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ListingsPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
