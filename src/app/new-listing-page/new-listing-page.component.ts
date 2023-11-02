9eacf27d6ea3f49e47854c83d6fbca3433798d66
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-11-02 21:49:17 +0530

    Implemnent New Listing Page

--- src/app/new-listing-page/new-listing-page.component.ts
+++ src/app/new-listing-page/new-listing-page.component.ts
@@ -1,8 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-new-listing-page",
   templateUrl: "./new-listing-page.component.html",
   styleUrls: ["./new-listing-page.component.css"],
 })
-export class NewListingPageComponent {}
+export class NewListingPageComponent {
+  name: string = "";
+  description: string = "";
+  price: string = "";
+
+  constructor(private router: Router) {}
+
+  onSubmit(): void {
+    alert("Creating a new listing");
+    this.router.navigateByUrl("/my-listings");
+  }
+}
